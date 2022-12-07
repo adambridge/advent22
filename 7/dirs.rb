d647aab6fb0700716d3acfa47aabf9bba356f676
Adam Bridge <bridge.adam@gmail.com>
2022-12-07 02:39:58 -0500

    Day 7 pt2

--- 7/dirs.rb
+++ 7/dirs.rb
@@ -11,5 +11,16 @@ file.readlines.each do |line|
     path.size.times { |i| sizes[path.first(i + 1).join('/')] += $1.to_i}
   end
 end
-sizes.select! { |name, size| size <= 100000 }
-puts sizes.map { |name, size| size }.sum
+under100k = sizes.select { |name, size| size <= 100000 }
+puts "Sum of dirs < 100k: #{under100k.map { |name, size| size }.sum}"
+used = sizes['/']
+puts "Space used: #{used}"
+free = 70000000 - used
+puts "Free space: #{free}"
+required = 30000000
+to_free = required - free
+puts "Space to be freed up: #{to_free}"
+big_enough = sizes.reject { |name, size| size < to_free }
+puts "Dirs that reclaim enough space: #{big_enough}"
+puts "Size of dir to delete: #{big_enough.map {|n,s| s }.min}"
+
